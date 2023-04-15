59bbd0b8dee40a3aca70463a96ca40ee4601f8cb
Newton Roncari <new123cesar@gmail.com>
2023-04-15 14:29:12 -0300

    init shared library

--- src/integration/IntegrationFlow.groovy
+++ src/integration/IntegrationFlow.groovy
@@ -4,30 +4,29 @@ import integration.utils.Commands
 
 class IntegrationFlow {
 
-    def myCommands
+    def commands
 
     IntegrationFlow(jenkinsFileContext) {
-        myCommands = new Commands(jenkinsFileContext)
+        commands = new Commands(jenkinsFileContext)
     }
 
     def build() {
-        myCommands.call("echo Building application")
-        sh 'echo Building application...'
+        commands.call("echo Building application")
         // myCommands.call("cd spring-boot-projeto")
         // myCommands.call("./gradlew build")
     }
 
-//    def runTests() {
-//        sh 'echo Running tests...'
-//        // myCommands.call("cd spring-boot-projeto")
-//        // myCommands.call("./gradlew test")
-//    }
-//
-//    def deploy() {
-//        sh 'echo Deploying application...'
-//        // myCommands.call("cd spring-boot-projeto")
-//
-//        //  def jarFileName = myCommands.call("ls build/libs/*.jar | tail -1 | xargs -n 1 basename").trim()
-//        // myCommands.call("java -jar spring-boot-projeto/build/libs/${jarFileName}")
-//    }
+   def runTests() {
+       commands.call("echo Building application")
+       // myCommands.call("cd spring-boot-projeto")
+       // myCommands.call("./gradlew test")
+   }
+
+   def deploy() {
+       commands.call("echo Building application")
+       // myCommands.call("cd spring-boot-projeto")
+
+       //  def jarFileName = myCommands.call("ls build/libs/*.jar | tail -1 | xargs -n 1 basename").trim()
+       // myCommands.call("java -jar spring-boot-projeto/build/libs/${jarFileName}")
+   }
 }
